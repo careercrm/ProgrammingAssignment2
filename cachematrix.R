d0e950e8e23fc0a81548eb2d08ecee34ce0ab74c
careercrm <careercrm@gmail.com>
2016-10-15 20:35:00 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,11 @@
-## A pair of functions that cache the inverse of a matrix
+## Caching the Inverse of a Matrix:
+## Matrix inversion is usually a costly computation and there may be some 
+## benefit to caching the inverse of a matrix rather than compute it repeatedly.
+## Below are a pair of functions that are used to create a special object that 
+## stores a matrix and caches its inverse.
 
 
-## Creates a special matrix object that can cache its inverse
+## This function creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -10,8 +14,8 @@ makeCacheMatrix <- function(x = matrix()) {
     i <- NULL
     
     ## Method to set the matrix
-    set <- function( matrix ) {
-      x <<- matrix
+    set <- function(y) {
+      x <<- y
       i <<- NULL
     }
     
@@ -40,29 +44,29 @@ makeCacheMatrix <- function(x = matrix()) {
   
 
 
-# Compute the inverse of the special matrix returned by "makeCacheMatrix" above. If the inverse has already been calculated (and the matrix has not
-## changed), then the "cachesolve" should retrieve the inverse from the cache.
+# Compute the inverse of the special matrix returned by "makeCacheMatrix" above.
+##If the inverse has already been calculated (and the matrix has not changed), then the "cachesolve" should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
   
   ## Return a matrix that is the inverse of 'x'
-  x <- x$getInverse()
+  i <- x$getInverse()
   
   ## Just return the inverse if its already set
-  if( !is.null(x) ) {
+  if( !is.null(i) ) {
     message("getting cached data")
-    return(x)
+    return(i)
   }
   
   ## Get the matrix from our object
   data <- x$get()
   
   ## Calculate the inverse using matrix multiplication
-  x <- solve(data) %*% data
+  i <- solve(data,...)
   
   ## Set the inverse to the object
-  x$setInverse(x)
+  x$setInverse(i)
   
   ## Return the matrix
-  x
+  i
 }
